c9a170f4fa0a79def8396cd846863d1112291944
pachr16 <32641694+pachr16@users.noreply.github.com>
2019-04-15 16:44:13 +0200

    updatePositions done
    title

--- Map/src/main/java/dk/sdu/g3/map/Map.java
+++ Map/src/main/java/dk/sdu/g3/map/Map.java
@@ -2,6 +2,7 @@ package dk.sdu.g3.map;
 
 import dk.sdu.g3.common.data.Coordinate;
 import dk.sdu.g3.common.data.ITile;
+import dk.sdu.g3.common.entities.IMovable;
 import dk.sdu.g3.common.services.IMap;
 import dk.sdu.g3.common.services.IPlaceableEntity;
 import java.util.ArrayList;
@@ -13,12 +14,11 @@ import org.openide.util.lookup.ServiceProviders;
 @ServiceProviders(value = { @ServiceProvider(service = IMap.class),})
 public class Map implements IMap {
 
-    private ArrayList<ITile> tiles = new ArrayList<ITile>();
+    private ArrayList<Tile> tiles = new ArrayList<Tile>();
     private int lengthX, lengthY;
     
     
     public Map() {
-        
     }
     
     public Map(int lengthX, int lengthY) {
@@ -29,7 +29,11 @@ public class Map implements IMap {
     
     @Override
     public List<ITile> getTileList() {
-        return this.tiles;
+        ArrayList<ITile> returnTiles = new ArrayList<ITile>();
+        for (Tile tile : tiles) {
+            returnTiles.add(tile);
+        }
+        return returnTiles;
     }
 
     @Override
@@ -43,13 +47,38 @@ public class Map implements IMap {
     }
 
     @Override
-    public List<IPlaceableEntity> updatePositions() {       // remember to remove entities as soon as x coordinate is greater than screen length
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<IPlaceableEntity> updatePositions() {
+        ArrayList<IPlaceableEntity> toBeRemoved = new ArrayList<IPlaceableEntity>();
+        
+        for (Tile tile : tiles) {
+            for (IPlaceableEntity entity : tile.getEntities()) {
+                if (entity instanceof IMovable) {
+                    
+                    tile.remove(entity);            // remove entity from the tile it is currently on
+                    Coordinate currentPos = entity.getCurrentPosition();
+                    Coordinate newPos = ((IMovable) entity).getNextStep(currentPos);
+                    
+                    if (newPos == null) {               // if entity has reached the end of its path, it should be despawned.
+                        toBeRemoved.add(entity);
+                        Coordinate endPos = new Coordinate(this.lengthX, currentPos.getY());    // unit is at the end of its path - it is put at the end of the map until a controller removes it.
+                        entity.setCurrentPosition(endPos);
+                        getTile(endPos).add(entity);        // place entity on end tile
+                        continue;
+                    }
+                    
+                    entity.setCurrentPosition(newPos);
+                    getTile(newPos).add(entity);        // place entity on new tile
+                    
+                }
+            }
+        }
+        
+        return toBeRemoved;     // returns list of the units that made it to the endzone
     }
 
     @Override
     public void updateActions() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -57,8 +86,8 @@ public class Map implements IMap {
         return getTile(coord).getEntities();
     }
     
-    private ITile getTile(Coordinate coord) {
-        for (ITile tile : tiles) {
+    private Tile getTile(Coordinate coord) {
+        for (Tile tile : tiles) {
             if (tile.getCoordinate().getX() - tile.getSize() < coord.getX() && tile.getCoordinate().getX() + tile.getSize() > coord.getX()
                     && tile.getCoordinate().getY() - tile.getSize() < coord.getY() && tile.getCoordinate().getY() + tile.getSize() > coord.getY()) {
                 return tile;
