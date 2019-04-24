b4a9cd70d28e4debc31f46e7e34c00cfaea0c2f7
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2019-04-24 15:33:37 +0200

    Neighbours
    Added neighbour assignment to Pathfinding and neighbour variables to Node

--- Pathfinding/Pathfinding/src/main/java/dk/sdu/g3/pathfinding/Node.java
+++ Pathfinding/Pathfinding/src/main/java/dk/sdu/g3/pathfinding/Node.java
@@ -6,28 +6,107 @@
 package dk.sdu.g3.pathfinding;
 
 import dk.sdu.g3.common.data.Coordinate;
-import dk.sdu.g3.common.data.ITile;
 
-/**
- *
- * @author Pottemuld
- */
+
 public class Node {
     private double accumulatedStepCost; //The cost accumulated from the start node to the current node
     private double heuristic; //estimated heuristic value, defined by the heuristic function
     private double totalPathCost; //accumulatedStepCost + heuristic - estimated total cost from start to goal through this node
-    private Coordinate center;
-    private int sizeX;
-    private int sizeY;
+    private final Coordinate center;
+    private final int size;
     private Node previousNode;
+    private Node leftNeighbour;
+    private Node rightNeighbour;
+    private Node upNeighbour;
+    private Node downNeightbour;
 
-    public Node(Coordinate center, int sizeX, int sizeY) {
-        this.center = center;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
+    
+    
+    
+    
+    
+    public Node getLeftNeighbour() {
+        return leftNeighbour;
+    }
+
+    public void setLeftNeighbour(Node leftNeighbour) {
+        this.leftNeighbour = leftNeighbour;
+    }
+
+    public Node getRightNeighbour() {
+        return rightNeighbour;
+    }
+
+    public void setRightNeighbour(Node rightNeighbour) {
+        this.rightNeighbour = rightNeighbour;
+    }
+
+    public Node getUpNeighbour() {
+        return upNeighbour;
+    }
+
+    public void setUpNeighbour(Node upNeighbour) {
+        this.upNeighbour = upNeighbour;
+    }
+
+    public Node getDownNeightbour() {
+        return downNeightbour;
+    }
+
+    public void setDownNeightbour(Node downNeightbour) {
+        this.downNeightbour = downNeightbour;
     }
     
     
+
+    public Node(Coordinate center, int size) {
+        this.center = center;
+        this.size = size;
+    }
+
+    
+    public void setAccumulatedStepCost(double accumulatedStepCost) {
+        this.accumulatedStepCost = accumulatedStepCost;
+    }
+
+    public void setHeuristic(double heuristic) {
+        this.heuristic = heuristic;
+    }
+
+    public void setTotalPathCost(double totalPathCost) {
+        this.totalPathCost = totalPathCost;
+    }
+
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
+    }
+
+    
+    
+    public double getAccumulatedStepCost() {
+        return accumulatedStepCost;
+    }
+
+    public double getHeuristic() {
+        return heuristic;
+    }
+
+    public double getTotalPathCost() {
+        return totalPathCost;
+    }
+
+    public Coordinate getCenter() {
+        return center;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Node getPreviousNode() {
+        return previousNode;
+    }
+ 
     
     
 }
--- Pathfinding/Pathfinding/src/main/java/dk/sdu/g3/pathfinding/Pathfinding.java
+++ Pathfinding/Pathfinding/src/main/java/dk/sdu/g3/pathfinding/Pathfinding.java
@@ -14,10 +14,15 @@ import java.util.List;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
 
-@ServiceProviders(value = {@ServiceProvider(service = IPathfinding.class), })
+@ServiceProviders(value = {
+    @ServiceProvider(service = IPathfinding.class),})
 public class Pathfinding implements IPathfinding {
 
+    IMap map;
+    int lenghtX = map.getLengthX();
+    int lengthY = map.getLengthY();
     List<Node> nodes = new ArrayList<>();
+
     @Override
     public Coordinate getNextCoordinate(Coordinate currentCoord) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -25,16 +30,62 @@ public class Pathfinding implements IPathfinding {
 
     @Override
     public List<Coordinate> generatePath(IMap map, Coordinate start, Coordinate goal) {
-       
+        return null;
+
     }
-    
-    private void createNodes(IMap map) {
+
+    private Node assignNeighbour(int x, int y) { //Look out for null pointer
+        for (Node node : nodes) {
+            if (node.getCenter().getX() == x) {
+                if (node.getCenter().getY() == y) {
+                    return node;
+                }
+            }
+        } //Possible null pointer
+        return null;
+    }
+
+    private void createNodes() {
         List<ITile> tiles = map.getTileList();
-        for(ITile tile : tiles) {
-            Node node = new Node(tile.getCoordinate(), tile.get);
+        for (ITile tile : tiles) {
+            Node node = new Node(tile.getCoordinate(), tile.getSize());
             nodes.add(node);
         }
     }
-    
-    
+
+    private void setAdjacentNodes(Node currentNode) {
+        setLeftNeighbour(currentNode);
+        setRightNeighbour(currentNode);
+        setUpNeighbour(currentNode);
+        setDownNeighbour(currentNode);
+    }
+
+    private void setLeftNeighbour(Node currentNode) {
+        //If (center coordinate of currentNode(x) - size of currentNode(x)) > min(x), we set left neighbour to be the node with center(x) = center(currentnode(x))-2 x size(x)
+        if ((currentNode.getCenter().getX() - currentNode.getSize()) > 0) {
+            currentNode.setLeftNeighbour(assignNeighbour((currentNode.getCenter().getX() - (currentNode.getSize() * 2)), currentNode.getCenter().getY()));
+        }
+    }
+
+    private void setRightNeighbour(Node currentNode) {
+        //If (center coordinate of currentNode(x) + size of currentNode(x) < max(x), we set right neighbour
+        if ((currentNode.getCenter().getX() + currentNode.getSize() < map.getLengthX())) {
+            currentNode.setRightNeighbour(assignNeighbour((currentNode.getCenter().getX() + (currentNode.getSize() * 2)), currentNode.getCenter().getY()));
+        }
+    }
+
+    private void setUpNeighbour(Node currentNode) {
+        //If (center coordinate of currentNode(y) - size of currentNode(y)) > min(y), we set up neighbour  
+        if((currentNode.getCenter().getY() - currentNode.getSize() > 0)) {
+            currentNode.setUpNeighbour(assignNeighbour(currentNode.getCenter().getX(), currentNode.getCenter().getY() - (currentNode.getSize() * 2)));
+        }
+    }
+
+    private void setDownNeighbour(Node currentNode) {
+        //If (center coordinate of currentNode(y) + size of currentNode(y)) < max(y), we set down neighbour  
+        if(currentNode.getCenter().getY() + currentNode.getSize() < map.getLengthY()) {
+            currentNode.setDownNeightbour(assignNeighbour(currentNode.getCenter().getX(), currentNode.getCenter().getY() + (currentNode.getSize() * 2)));
+        }
+    }
+
 }
