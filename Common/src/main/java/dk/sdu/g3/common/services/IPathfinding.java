cf5ccb1b61bbb53a714e827411c04b5484389c34
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2019-05-01 13:54:57 +0200

    Removed getNextCoordinate method from Pathfinding and IPathFinding

--- Common/src/main/java/dk/sdu/g3/common/services/IPathfinding.java
+++ Common/src/main/java/dk/sdu/g3/common/services/IPathfinding.java
@@ -14,6 +14,5 @@ import java.util.List;
  */
 public interface IPathfinding {
     
-//    Coordinate getNextCoordinate(Coordinate currentCoord);
     List<Coordinate> generatePath(IMap map, Coordinate start, Coordinate goal) throws Exception; 
 }
