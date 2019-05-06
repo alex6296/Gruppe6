cc8c8aea9174697964775a32d3bb4519ed52b9be
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2019-05-06 16:23:54 +0200

    Game loop and sendwave + mainmenu button added

--- Common/src/main/java/dk/sdu/g3/common/rendering/GraphicsMap.java
+++ Common/src/main/java/dk/sdu/g3/common/rendering/GraphicsMap.java
@@ -16,6 +16,8 @@ public class GraphicsMap {
         graphics.put(Graphic.TowerPickerBackground, "assets/Par1.png");
         graphics.put(Graphic.Towers, "assets/SmallStatue.png");
         graphics.put(Graphic.Coins, "assets/Coin.png");
+        graphics.put(Graphic.MAIN_MENU, "assets/MainMenu.png");
+        graphics.put(Graphic.SEND_WAVE, "assets/sendWave.png");
     }
 
     public HashMap<Graphic, String> getGraphics() {
