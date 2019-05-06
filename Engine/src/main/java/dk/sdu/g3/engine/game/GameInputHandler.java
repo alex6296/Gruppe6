b642be5c5b96d9c39a1012185293558ee04ad96a
Rasmus Jensen <rasje17@student.sdu.dk>
2019-05-06 11:44:17 +0200

    imputhandler ½don

--- Engine/src/main/java/dk/sdu/g3/engine/game/GameInputHandler.java
+++ Engine/src/main/java/dk/sdu/g3/engine/game/GameInputHandler.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.InputAdapter;
 import dk.sdu.g3.common.data.Coordinate;
 import dk.sdu.g3.common.data.ITile;
 import dk.sdu.g3.common.rendering.IStage;
+import dk.sdu.g3.common.services.ITower;
 
 
 public class GameInputHandler extends InputAdapter {
@@ -27,11 +28,22 @@ public class GameInputHandler extends InputAdapter {
 
     private boolean clickIsOnStage(int x, int y) {
         for(IStage stage : game.getStages()){
-            if(stageIsClicked(stage,x,y))
-                if(stageHandleClick(stage,x,y) instanceof ITile){
-                    
+            if(stageIsClicked(stage,x,y)){
+                Object ret = stageHandleClick(stage,x,y);
+                if(ret instanceof ITile){
+                    if(((ITile) ret).isOccupied()){
+                        //inspect ret
+                        //((ITile) ret).getEntities()
+                                           }
+                    else{
+                        //player.createTower(((ITile) ret).getCoordinate());
+                    }
+                }
+                else if (ret instanceof ITower){
+                    //inspect Tower eller set reserved tower
                 }
             return true;
+            }
         }
 
         return false;
